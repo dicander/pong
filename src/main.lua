9e8a641cf17fb93125b642c1480a26d072d8bad9
Marcus Dicander <dicander@gmail.com>
2016-05-22 00:19:11 +0200

    Improved the pad bouncing accuracy and made the ball round.

--- src/main.lua
+++ src/main.lua
@@ -11,6 +11,7 @@ boardheight = 600
 boardwidth = 800
 p1score = 0
 p2score = 0
+paused = false
 
 function newgame()
     p1 = boardheight / 2 - padheight/2
@@ -57,22 +58,42 @@ function move_ball()
     elseif ball_x >= boardwidth then
         p1score = p1score + 1
         newgame()
-    elseif 750-10 <= ball_x and ball_x <=750+10 and ball_x_speed >0 and p2-10 <= ball_y and ball_y <= p2+padheight then
+    elseif 750 <= ball_x+ball_x_speed and ball_x <=750 and ball_x_speed >0 and p2-10 <= ball_y and ball_y <= p2+padheight then
+        part =((750-ball_x)/ball_x_speed)
+        ball_x = ball_x + part*ball_x_speed
+        ball_y = ball_y + part*ball_y_speed
         ball_x_speed = -ball_x_speed -1
-        ball_y_speed = (p2+padheight/2 - ball_y)/10
-     elseif 50-10 <= ball_x and ball_x <=50+10 and ball_x_speed < 0 and p1-10 <= ball_y and ball_y <= p1+padheight then
-        ball_x_speed = -ball_x_speed + 1
-        ball_y_speed = (p1+padheight/2 - ball_y)/10
-    else
+        ball_y_speed = -(p2+padheight/2-ball_y)/10
+        ball_x = ball_x - part*ball_x_speed
+        ball_y = ball_y - part*ball_y_speed
+     elseif ball_x+ball_x_speed <= 50 and ball_x_speed < 0 and p1-10 <= ball_y and ball_y <= p1+padheight then
+        part = ((ball_x-50)/ball_x_speed)
+        ball_x = ball_x + part*ball_x_speed
+        ball_y = ball_y + part*ball_y_speed
         ball_x = ball_x + ball_x_speed
         ball_y = ball_y + ball_y_speed
+        ball_x_speed = -ball_x_speed + 1
+        ball_y_speed = -(p1+padheight/2-ball_y)/10
+        ball_x = ball_x - part*ball_x_speed
+        ball_y = ball_y - part*ball_y_speed
     end
+    ball_x = ball_x + ball_x_speed
+    ball_y = ball_y + ball_y_speed
     if ball_y <= 0 or ball_y >= boardheight -10 then
         ball_y_speed = - ball_y_speed
     end
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("escape") then
+        love.event.quit()
+    end
+    if love.keyboard.isDown("p") then
+        paused = not paused
+    end
+    if paused then
+        return
+    end
     dp1 = playerupdate("e", "d", dp1)
     dp2 = playerupdate("up", "down", dp2)
     p1, dp1 = move_and_checkfloors(p1, dp1) 
@@ -80,11 +101,13 @@ function love.update(dt)
     move_ball()
 end
     
-
 function love.draw()
+    if paused then
+        love.graphics.print("PAUSED", 380, 50+(ball_y+400)%500)
+    end
     love.graphics.rectangle('fill', 40, p1, 10, padheight)
     love.graphics.rectangle('fill', 750, p2, 10, padheight)
-    love.graphics.rectangle('fill', ball_x, ball_y, 10, 10)
+    love.graphics.circle('fill', ball_x+5, ball_y+5, 5, 100)
     love.graphics.print(""..tostring(p1score).." - "..tostring(p2score), boardwidth/2-30, 40)
     --love.graphics.print("bxs:"..tostring(ball_x_speed).." bys:"..tostring(ball_y_speed), 300, 400)
     --love.graphics.print("p1:"..tostring(p1).." p2:"..tostring(p2).." ", 300, 450)
